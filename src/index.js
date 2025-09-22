1df933134a90df135323265b207bc87e674ac7ec
weizhixie <wx02697@yahoo.co.uk>
2025-09-22 17:43:02 +0100

    Implement tabbed browsing feature

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import { loadHomePage } from "./homepage.js";
 import { loadMenuPage } from "./menu-page.js";
 
 (() => {
+    const content = document.querySelector("#content");
+    const navBtns = document.querySelectorAll(".nav-btn");
+    const pages = {
+        "home-btn": loadHomePage,
+        "menu-btn": loadMenuPage,
+    };
+
+    navBtns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            if (pages[e.currentTarget.id]) {
+                content.textContent = "";
+                pages[e.currentTarget.id]();
+            } else {
+                console.warn("Sorry unknown page", e.currentTarget.id);
+            }
+        });
+    });
+
     loadHomePage();
-    loadMenuPage();
 })();
